701ecb0ab77c0b933bf66445b8ca04dd3efde280
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-08 15:37:37 -0500

    Integración de el restcontroller

--- src/main/java/com/milenyumsoft/springsecurity/controller/HelloWorldController.java
+++ src/main/java/com/milenyumsoft/springsecurity/controller/HelloWorldController.java
@@ -1,7 +1,21 @@
 package com.milenyumsoft.springsecurity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+
+
+    @GetMapping("holaseg")
+    public String secHelloWorld(){
+        return "Hola Mundo Mylenyum-Soft con seguridad";
+    }
+
+
+    @GetMapping("/holanoseg")
+    public String noSecHelloWorld(){
+        return "Hola mundo Mylenuum-Soft sin seguridad" ;
+    }
 }
