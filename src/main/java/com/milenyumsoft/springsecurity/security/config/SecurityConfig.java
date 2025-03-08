0244a077e215d64b9ec7eeb00eb6d2c6dc5f44d8
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-03-08 16:53:04 -0500

    Integration completed from all method of security

--- src/main/java/com/milenyumsoft/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/com/milenyumsoft/springsecurity/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -28,7 +29,7 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(csrf->csrf.disable())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session->session.sessionCreationPolicy.STATELESS())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(http ->{
                     //Endpoints
 
